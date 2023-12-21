c19d634e77af55789e3541ae086c5acc965faebf
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-21 15:48:02 +0200

    Use put method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -6,11 +6,27 @@ app = Flask(__name__)
 api = Api(app)
 
 
+todos = {
+    'todo1': 'task1',
+    'todo2': 'task2',
+    'todo3': 'task3',
+}
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
     
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 if __name__ == '__main__':
