9b0870883bf731ad2cb73e77aed4168d93a68f15
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-21 22:36:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort, reqparse
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-todos = {
-    'todo1': 'task1',
-    'todo2': 'task2',
-    'todo3': 'task3',
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
 }
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
 class HelloWorld(Resource):
     def get(self):
-        return todos
+        return TODOS
 
 
 class TodoSimple(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
     
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
     
     def delete(self, todo_id):
-        del todos[todo_id]
-        return 'data deleted', 204
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(TodoSimple, '/<string:todo_id>')
