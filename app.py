f5a1918b400b4e4897726e5265b3e8db3fe4e0d4
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-24 23:06:23 +0200

    Testing swagger

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, abort, reqparse
 
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+
 api = Api(app)
 
 
@@ -46,4 +70,5 @@ api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 if __name__ == '__main__':
+    app.register_blueprint(swaggerui_blueprint)
     app.run(debug=True)
